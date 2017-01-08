7710ba53174067be532c251c57bd83968932e3b9
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2017-01-08 12:46:42 +0100

    Initial Commit

---
+++ OverPing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OverPing
+//
+//  Created by Nevyn Bengtsson on 1/8/17.
+//  Copyright © 2017 Nevyn Bengtsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
