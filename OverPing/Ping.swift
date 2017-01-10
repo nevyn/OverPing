3dae410af2efa0ff63f1be67f7f5f48cb430d485
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2017-01-10 14:07:47 -0800

    clean up ping nested blocks

--- OverPing/Ping.swift
+++ OverPing/Ping.swift
@@ -33,45 +33,50 @@ class Ping: NSObject {
         task.arguments = ["-c", "1", "8.8.8.8"]
         task.standardOutput = standardOut
         task.standardError = standardErr
-        task.terminationHandler = { task in
-            let out = NSString(data: self.standardOut.fileHandleForReading.readDataToEndOfFile(), encoding: String.Encoding.utf8.rawValue)!
-            let err = NSString(data: self.standardErr.fileHandleForReading.readDataToEndOfFile(), encoding: String.Encoding.utf8.rawValue)!
-            DispatchQueue.main.async {
-                guard err.length == 0 else {
-                    self.error = .unknown(reason: err as String)
-                    self.delegate?.ping(self, failedWithError: self.error!)
-                    return
-                }
-                let lines = out.components(separatedBy: "\n")
-                guard lines.count >= 6 else {
-                    self.error = .unknown(reason: out as String)
-                    self.delegate?.ping(self, failedWithError: self.error!)
-                    return
-                }
-                
-                let pingLine = lines[1]
-                
-                guard !pingLine.hasPrefix("Request timeout") else {
-                    self.error = .pingTimeout
-                    self.delegate?.ping(self, failedWithError: self.error!)
-                    return
-                }
-                
-                let msRegex = try! NSRegularExpression(pattern: "64 bytes from 8.8.8.8: icmp_seq=0 ttl=.+ time=(.+) ms")
-                guard let results = msRegex.firstMatch(in: out as String, range: NSRange(location: 0, length: out.length)) else {
-                    self.error = .unknown(reason: out as String)
-                    self.delegate?.ping(self, failedWithError: self.error!)
-                    return
-                }
-                let msString = out.substring(with: results.rangeAt(1)) as NSString
-                let secs = msString.doubleValue/1000.0
-                
-                self.secs = secs
-                self.delegate?.ping(self, didFinishWithTime: secs)
-            }
-            
-        }
+        task.terminationHandler = terminationHandler
         task.launch()
+    } // run()
+    
+    func terminationHandler(task : Process) {
+        let out = NSString(data: self.standardOut.fileHandleForReading.readDataToEndOfFile(), encoding: String.Encoding.utf8.rawValue)!
+        let err = NSString(data: self.standardErr.fileHandleForReading.readDataToEndOfFile(), encoding: String.Encoding.utf8.rawValue)!
+        DispatchQueue.main.async {
+            self.handleEndOnMain(out: out, err: err)
+        }
+    }
+    func handleEndOnMain(out: NSString, err: NSString)
+    {
+        guard err.length == 0 else {
+            self.error = .unknown(reason: err as String)
+            self.delegate?.ping(self, failedWithError: self.error!)
+            return
+        }
+        let lines = out.components(separatedBy: "\n")
+        guard lines.count >= 6 else {
+            self.error = .unknown(reason: out as String)
+            self.delegate?.ping(self, failedWithError: self.error!)
+            return
+        }
+        
+        let pingLine = lines[1]
+        
+        guard !pingLine.hasPrefix("Request timeout") else {
+            self.error = .pingTimeout
+            self.delegate?.ping(self, failedWithError: self.error!)
+            return
+        }
+        
+        let msRegex = try! NSRegularExpression(pattern: "64 bytes from 8.8.8.8: icmp_seq=0 ttl=.+ time=(.+) ms")
+        guard let results = msRegex.firstMatch(in: out as String, range: NSRange(location: 0, length: out.length)) else {
+            self.error = .unknown(reason: out as String)
+            self.delegate?.ping(self, failedWithError: self.error!)
+            return
+        }
+        let msString = out.substring(with: results.rangeAt(1)) as NSString
+        let secs = msString.doubleValue/1000.0
+        
+        self.secs = secs
+        self.delegate?.ping(self, didFinishWithTime: secs)
     }
 }
 
